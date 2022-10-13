5b1f068cf87c90e2cadb11a870618e2e22b49dca
asiabeoon <a.hashbey@gmail.com>
2022-10-12 20:07:41 -0400

    Adding final Code JQuery

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+//Puts Current date under the Curent day paragraph header
+var todayDate = moment().format("MMM Do YY");
+$("#currentDay").text(todayDate);
+
+
+//Add Event Listener "click" to Save User Input to local Storage
+
+$(".saveBtn").on("click", function () {
+//  localStorage.setItem$(this).prop("id"),
+//  console.log(localStorage.getItem $(this).prop("id")));
+
+
+//this is for Save buttons sibling is description, parent id =hour 9(ex)
+var text =$(this).siblings(".description").val()
+var time =$(this).parent().attr("id")
+console.log(time, text)
+
+localStorage.setItem(time, text)
+})
+
+$("#hour9 .description").val(localStorage.getItem("hour9"))
+$("#hour10 .description").val(localStorage.getItem("hour10"))
+$("#hour11 .description").val(localStorage.getItem("hour11"))
+$("#hour12 .description").val(localStorage.getItem("hour12"))
+$("#hour13 .description").val(localStorage.getItem("hour13"))
+$("#hour14 .description").val(localStorage.getItem("hour14"))
+$("#hour15 .description").val(localStorage.getItem("hour15"))
+$("#hour16 .description").val(localStorage.getItem("hour16"))
+$("#hour17 .description").val(localStorage.getItem("hour17"))
+
+$(".description").each(function(){
+    console.log(this)
+    //parseInt turns moment into a number from string
+    var currentHour = parseInt(moment().hour())
+    //added a variable and use the sttribute id="17"(ex) then parsed the string to a numberfor the row hour
+    var rowHour = parseInt($(this).attr("id"))
+
+    if(currentHour < rowHour){
+$(this).addClass("past")
+
+    }
+    else if(currentHour===rowHour) {
+        $(this).removeClass("past")
+        $(this).addClass("present")
+    }
+    else {
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+        $(this).addClass("future")
+
+    
+    }
+}) 
+
+//(ex) = example
+
+
+
+
+
+
+   
